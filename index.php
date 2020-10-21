758a7565e2936c173f2233ecafd9c2c49b975ba0
yu-kappa <61277151+yu-kappa@users.noreply.github.com>
2020-10-21 11:01:12 +0900

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php get_header(); ?>
+
+	<h2 class="pageTitle">最新情報<span>NEWS</span></h2>
+
+	<?php get_template_part('template-parts/breadcrumb'); ?>
+
+	<main class="main">
+		<div class="container">
+			<div class="posts">
+				<div class="post_page">
+					<?php if (is_month()): ?>
+						<h2 class="main_title"><?php the_time('Y年m月'); ?></h2>
+					<?php else: ?>
+						<h2 class="main_title"><?php wp_title(''); ?></h2>
+					<?php endif; ?>
+
+					<div class="posts">
+						<?php if (have_posts()): ?>
+							<?php while (have_posts()): the_post(); ?>
+								<div class="post">
+									<?php get_template_part('template-parts/loop', 'news'); ?>
+								</div>
+							<?php endwhile; ?>
+						<?php endif; ?>
+							
+
+					</div>
+				</div>
+
+				<div class="side_space">
+					<?php get_sidebar('categories'); ?>
+					<?php get_sidebar('archives'); ?>
+				</div>
+			</div>
+		</div>
+	</main>
+
+<?php get_footer(); ?>
